4c97c9e28e86dd4a8fb59cf955ed1c54f050e95a
Guilherme-Miranda-Carvalho <89986829+Guilherme-Miranda-Carvalho@users.noreply.github.com>
2021-09-30 20:59:50 -0300

    Create pt1_menu.py

---
+++ pt1_menu.py
@@ -0,0 +1,24 @@
+# inicio menu
+def menu():
+    num = int
+    print("\n-------------Menu-------------")
+    print("\nDigite um numero correspondente ao menu: \n[1] - Cadastrar novo usuário \n[2] - Exibir todos usuários por ordem de cadastro \n[3] - Exibi todos usuários por ordem alfabética \n[4] - Buscar usuário \n[5] - Remover um usuário \n[6] - Alterar um usuário")
+    num = int(input())
+    if (num == 1):
+        pass
+    elif (num == 2):
+        pass
+    elif (num == 3):
+        pass
+    elif (num == 4):
+        pass
+    elif (num == 5):
+        pass
+    elif (num == 6):
+        pass
+    else:
+        print("Numero digitado não corresponde as opções do menu, digite um numero valido!")
+# fim  menu
+
+if __name__ == "__main__":
+    menu() 
