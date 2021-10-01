d5d2ebbefb5eeb70a87387b549998e604a8c2dc7
tainassilva <88388962+tainassilva@users.noreply.github.com>
2021-09-30 21:04:05 -0300

    Create teste_n1.py
    Essa é uma versão do código e ainda tem funções a serem implementadas

---
+++ teste_n1.py
@@ -0,0 +1,46 @@
+def criarUsuario():  # Função para criar usuário
+    novoUsuario= {} 
+    novoUsuario["nomeCompleto"] = input('Digite o seu nome completo: ')
+    novoUsuario["email"] = input('Digite o seu email: ')
+    return novoUsuario #Vai retornar o dicionário   
+
+def exibirNomeAlunosCadastrados(listaUsuarioCadastrado: list):
+    for umUsuario in listaUsuarioCadastrado:
+        print("Nome do usuário: ", umUsuario["nomeCompleto"])
+        print("Email: ", umUsuario["email"])
+
+def exibirNomesAlunosOrdemAlfabetica(listaUsuarioCadastrado: list):
+    ordenada = list(listaUsuarioCadastrado)
+    ordenada.sort()
+    for key in ordenada:
+        print(key)
+
+def verificarSeExisteAluno(listaUsuarioCadastrado):
+    verificacao = input("Digite o nome que deseja pesquisar: ")
+    if listaUsuarioCadastrado["nomeCompleto"] == verificacao:
+        print("Aluno matriculado na instituição")
+    else :
+        print('Não está matriculado na instituição')
+        
+    
+    def main():
+    
+    listaUsuarioCadastrado = []
+
+    print('Cadastro do usuário')
+    listaUsuarioCadastrado.append(criarUsuario())
+
+    novocadastro = input('Gostaria de cadastrar um novo usuário? Digite 1 para sim e 2 para Não. ')
+    while novocadastro == "1":
+        listaUsuarioCadastrado.append(criarUsuario())
+        novocadastro = input('Gostaria de cadastrar um novo usuário? Digite 1 para sim e 2 para Não. ')
+
+    exibirNomeAlunosCadastrados(listaUsuarioCadastrado)
+    verificarSeExisteAluno(listaUsuarioCadastrado)
+    exibirNomesAlunosOrdemAlfabetica(listaUsuarioCadastrado)
+   
+    
+
+
+if __name__ == "__main__":
+    main()
